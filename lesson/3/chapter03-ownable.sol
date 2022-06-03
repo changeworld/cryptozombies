df1e1cd78f0a4d3fa323f55ac7f1cacc0c3d2c54
Takashi Takebayashi <changesworlds@gmail.com>
2022-06-04 01:15:40 +0900

    Tweak

--- lesson/3/chapter03-ownable.sol
+++ lesson/3/chapter03-ownable.sol
@@ -20,7 +20,6 @@ contract Ownable {
         require(msg.sender == owner);
         _;
     }
-
     /**
      * @dev Allows the current owner to transfer control of the contract to a newOwner.
      * @param newOwner The address to transfer ownership to.
