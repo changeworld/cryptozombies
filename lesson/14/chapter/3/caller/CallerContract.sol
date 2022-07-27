6d35c9f5e9c0881f0ab788e8d37b9f65fb5c5353
Takashi Takebayashi <changesworlds@gmail.com>
2022-07-27 12:31:55 +0900

    Add lesson 14 chapter 3

---
+++ lesson/14/chapter/3/caller/CallerContract.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.5.0;
+import "./EthPriceOracleInterface.sol";
+contract CallerContract {
+    EthPriceOracleInterface private oracleInstance;
+    address private oracleAddress;
+    function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+        oracleAddress = _oracleInstanceAddress;
+        oracleInstance = EthPriceOracleInterface(oracleAddress);
+    }
+}
