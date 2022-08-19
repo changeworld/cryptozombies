28037cfdaa16e6ba60652d7c0f5100d69479f120
Takashi Takebayashi <changesworlds@gmail.com>
2022-08-20 05:33:10 +0900

    Add lesson 16 chapter 6

---
+++ lesson/16/chapter/6/oracle/EthPriceOracle.sol
@@ -0,0 +1,63 @@
+pragma solidity 0.5.0;
+import "openzeppelin-solidity/contracts/access/Roles.sol";
+import "./CallerContractInterface.sol";
+contract EthPriceOracle {
+    using Roles for Roles.Role;
+    Roles.Role private owners;
+    Roles.Role private oracles;
+    uint private randNonce = 0;
+    uint private modulus = 1000;
+    uint private numOracles = 0;
+    mapping(uint256=>bool) pendingRequests;
+    struct Response {
+        address oracleAddress;
+        address callerAddress;
+        uint256 ethPrice;
+    }
+    mapping (uint256=>Response[]) public requestIdToResponse;
+    event GetLatestEthPriceEvent(address callerAddress, uint id);
+    event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+    event AddOracleEvent(address oracleAddress);
+    event RemoveOracleEvent(address oracleAddress);
+    constructor (address _owner) public {
+        owners.add(_owner);
+    }
+
+    function addOracle (address _oracle) public {
+        require(owners.has(msg.sender), "Not an owner!");
+        require(!oracles.has(_oracle), "Already an oracle!");
+        oracles.add(_oracle);
+        numOracles++;
+        emit AddOracleEvent(_oracle);
+    }
+
+    function removeOracle (address _oracle) public {
+        require(owners.has(msg.sender), "Not an owner!");
+        require(oracles.has(_oracle), "Not an oracle!");
+        require (numOracles > 1, "Do not remove the last oracle!");
+        oracles.remove(_oracle);
+        numOracles--;
+        emit RemoveOracleEvent(_oracle);
+    }
+
+    function getLatestEthPrice() public returns (uint256) {
+        randNonce++;
+        uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
+        pendingRequests[id] = true;
+        emit GetLatestEthPriceEvent(msg.sender, id);
+        return id;
+    }
+
+    function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public {
+        require(oracles.has(msg.sender), "Not an oracle!");
+        require(pendingRequests[_id], "This request is not in my pending list.");
+        Response memory resp;
+        resp = Response(msg.sender, _callerAddress, _ethPrice);
+        requestIdToResponse[_id].push(resp);
+        delete pendingRequests[_id];
+        CallerContractInterface callerContractInstance;
+        callerContractInstance = CallerContractInterface(_callerAddress);
+        callerContractInstance.callback(_ethPrice, _id);
+        emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
+    }
+}
